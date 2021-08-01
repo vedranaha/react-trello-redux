292ac18d01f4a467796d8d0b801461dc38ac5a13
Vedranaha <vedranaha@gmail.com>
2021-08-01 13:33:30 +0200

    components,actions, reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Controls from "./components/Controls.js";
+import TodosList from "./components/TodosList.js";
+import "./App.css";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">React Trello Clone</div>
+      <Controls />
+      <TodosList />
     </div>
   );
 }
