93ec1447d26930e12732ac0a59aee03686af432f
Vedranaha <vedranaha@gmail.com>
2021-08-15 21:05:26 +0200

    installed dnd, imported DragDropContext

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Controls from "./components/Controls.js";
 import TodosList from "./components/TodosList.js";
 import Board from "./components/Board";
 import "./App.css";
+import { DragDropContext } from "react-beautiful-dnd";
 
 function App(props) {
   return (
-    <div className="App">
-      <div className="Header">React Trello</div>
-      <Controls />
-      <Board />
-    </div>
+    <DragDropContext>
+      <div className="App">
+        <div className="Header">React Trello</div>
+        <Controls />
+        <Board />
+      </div>
+    </DragDropContext>
   );
 }
 
